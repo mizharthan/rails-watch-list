5ae2c69a3770f89bddd450e1847341ec642549ad
Mizuki Harthan <mizharthan@gmail.com>
2025-07-16 00:42:29 +0900

    gotem

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
   has_many :lists, through: :bookmarks
+  has_one_attached :poster
 
   validates :title, presence: true
   validates :overview, presence: true
